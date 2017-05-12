1c352472ba7c477644fcec33fd8c4256d3f21f93
Jay Hostan <hello@jayhostan.com>
2017-05-12 14:31:50 +0000

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -3,23 +3,45 @@ import React from 'react';
 export default class LoginForm extends React.Component {
   constructor() {
     super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+        userName: null,
+        password: null
+    };
+  }
 
-    this.state = {};
+  handleChange(field, e) {
+      this.setState({[field]: e.target.value});
+  }
+
+  handleSubmit(e) {
+      e.preventDefault();
+
+      const userName = this.state.userName;
+      const password = this.state.password;
+
+      if (userName && password) {
+          this.props.onSubmit({
+              userName,
+              password
+          });
+      }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.userName} onChange={this.handleChange.bind(this, 'userName')} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handleChange.bind(this, 'password')} />
           </label>
         </div>
         <div>
--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -1,17 +1,44 @@
 import React from 'react';
 
+function countWords(line) {
+  return line.split(' ').filter(l => l).length;
+}
+
+function validate(poem) {
+  const poemLines = poem.split('\n').filter(l => l);
+  const isRightAmountOfLines = poemLines.length === 3;
+  const hasRightAmountOfWords = countWords(poemLines[0]) === 5 && countWords(poemLines[1]) === 3 && countWords(poemLines[2]) === 5;
+  return isRightAmountOfLines && hasRightAmountOfWords;
+}
+
 export default class PoemWriter extends React.Component {
   constructor() {
     super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+        poem: null,
+        isValid: false
+    };
+  }
 
-    this.state = {};
+  handleChange(e) {
+      const text = e.target.value;
+      if (text) {
+          this.setState({
+              poem: text,
+              isValid: validate(text)
+          });
+      }
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.poem} onChange={this.handleChange} />
+        {!this.state.isValid?
+            <div id="poem-validation-error" style={{color: 'red'}} >This poem is not written in the right structure!</div> : null
+        }
+
       </div>
     );
   }
