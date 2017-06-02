23d7e7980f936e05e32dc14df5c87dc76289abab
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-06-02 02:21:23 +0000

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,25 +1,47 @@
 import React from 'react';
 
 export default class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.changeUsername = this.changeUsername.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  changeUsername(event) {
+    this.setState({username: event.target.value.trim()});
+  }
+
+  changePassword(event) {
+    this.setState({password: event.target.value.trim()});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let isValid = this.state.username.length > 0 && this.state.password.length > 0;
+    if (isValid)
+      this.props.onSubmit();
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" onChange={this.changeUsername} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" onChange={this.changePassword} value={this.state.password} />
           </label>
         </div>
         <div>
@@ -29,3 +51,7 @@ export default class LoginForm extends React.Component {
     );
   }
 }
+
+LoginForm.propTypes = {
+  onSubmit : React.PropTypes.func.isRequired,
+};
