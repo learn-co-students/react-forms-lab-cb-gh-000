b6c866404f1e8d0318b5bcceb5ae4cbf23aef628
charlyjazz <carlosjazzc1@gmail.com>
2017-10-31 13:07:01 -0400

    Done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,47 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  onSubmitProxy = event => {
+    event.preventDefault();
+    if (this.state.username.trim().length && this.state.password.trim().length) {
+      this.props.onSubmit({username: this.state.username, password:this.state.password})      
+    }
+  }
+
+  handleInputChange = event => {
+    if (event.target.name === 'password') {
+      this.setState({
+        password: event.target.value
+      });
+    }
+    else if (event.target.name === 'username') {
+      this.setState({
+        username: event.target.value
+      });
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmitProxy}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" name="username"
+                   value={this.state.username} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" name="password"
+                   value={this.state.password} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,26 +1,69 @@
 import React from 'react';
 
+const ErrorMessage = () => {
+  return <div id="poem-validation-error" style={{color: 'red'}}>
+            This poem is not written in the right structure!
+         </div>
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: '',
+      is_valid: false
+    };
+  }
+
+  handleTextArea = event => {
+    this.setState({poem: event.target.value})
+
+    if (this.TextAreaIsValid(event.target.value.split(/\r*\n/))) {
+      this.setState({is_valid: true})
+    }
+  }
+
+  TextAreaIsValid = array => {
+    let is_valid = true;
+
+    let checkLengthWord = (string, length) => {
+      return string.split(/\s+/).length === length
+    }
+
+    if (array.length === 3) {
+      array.forEach(function(element, index) {
+        if (index === 0) {
+          if (!checkLengthWord(element, 5)) {
+            is_valid = false
+          } 
+        }
+        else if (index === 1) {
+          if (!checkLengthWord(element, 3)) {
+            is_valid = false
+          } 
+        }
+        else if (index === 2) {
+          if (!checkLengthWord(element, 5)) {
+            is_valid = false
+          } 
+        }
+      }, this);
+    }
+    else {
+      is_valid = false;
+    }
+
+    return is_valid;
   }
 
   render() {
     return (
       <div>
-        <textarea
-          rows="3"
-          cols="60"
-        />
-        <div
-          id="poem-validation-error"
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
-      </div>
+        <textarea rows="3"cols="60" value={this.state.poem} 
+                  onChange={this.handleTextArea}/>
+        { !this.state.is_valid ? <ErrorMessage /> : null }
+      </div>  
     );
   }
 }
